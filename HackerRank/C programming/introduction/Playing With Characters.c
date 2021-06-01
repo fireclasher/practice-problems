71798816358a18f0abfb6a97fda9d4fb68b5f28a
fireclasher <akileswar2002@protonmail.com>
2021-06-01 19:53:20 +0530

    playing with characters

---
+++ HackerRank/C programming/introduction/Playing With Characters.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char ch ,s[20], sen[100];
+    scanf("%c", &ch);
+    scanf("%s", s);
+    scanf("\n");
+    scanf("%[^\n]%*c", sen);
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+   return 0; 
+}
